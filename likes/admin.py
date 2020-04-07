a237550f7e83f013c34f5c7996572da175213997
llllyyyhhhh <32054393+llllyyyhhhh@users.noreply.github.com>
2020-04-07 13:31:33 +0800

    初始化项目

---
+++ likes/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import LikeCount
+
+# Register your models here.
+@admin.register(LikeCount)
+class LikeCountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content_object', 'liked_num')
