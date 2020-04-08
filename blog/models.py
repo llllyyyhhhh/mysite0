52e4c83bc820ebb568ea4e6f96a274f1a73a6d1b
llllyyyhhhh <yf2913128@gmail.com>
2020-04-08 20:54:23 +0800

    debug

--- blog/models.py
+++ blog/models.py
@@ -3,23 +3,22 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from read_statistics.models import ReadNumExpandMethod, ReadDetail
 from django.contrib.contenttypes.fields import GenericRelation
-from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
 class BlogType(models.Model):
-    type_name = models.CharField(max_length=15)
+    type_name = models.CharField(max_length=15, verbose_name='类型名称')
 
     def __str__(self):
         return self.type_name
 
 class Blog(models.Model, ReadNumExpandMethod):
-    title = models.CharField(max_length=50)
-    blog_type = models.ForeignKey(BlogType, on_delete=models.CASCADE, related_name='blog_blog')
+    title = models.CharField(max_length=50, verbose_name='标题')
+    blog_type = models.ForeignKey(BlogType, on_delete=models.CASCADE, related_name='blog_blog', verbose_name='类型')
     content = RichTextUploadingField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
     read_details = GenericRelation(ReadDetail)
-    created_time = models.DateTimeField(auto_now_add=True)
+    created_time = models.DateTimeField(verbose_name='影片发行时间')
     last_updated_time = models.DateTimeField(auto_now=True)
     '''
     def get_read_num(self):
